765f983ac7995b00485ebbbabf858e5b1fdd2856
johnmal2012 <www.johnmal@gmail.com>
2026-06-19 10:11:42 -0400

    add debug to db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,6 +2,12 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from '@/db/schema';
 
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL is missing');
+}
+
 const sql = neon(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, {
