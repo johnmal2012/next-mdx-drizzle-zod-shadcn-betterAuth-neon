ded7aa8b76dca1753ac7b6ef22e1a1769264fb35
johnmal2012 <www.johnmal@gmail.com>
2026-06-26 12:54:35 -0400

    remove nodemailer reference

--- src/lib/_mail.ts
+++ src/lib/_mail.ts
@@ -1,12 +1,12 @@
 // use nodemailer and gmail to send verification email
-import nodemailer from 'nodemailer';
-import { serverEnv } from '@/lib/env/server';
+// import nodemailer from 'nodemailer';
+// import { serverEnv } from '@/lib/env/server';
 
-export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+// export const transporter = nodemailer.createTransport({
+//   service: 'gmail',
 
-  auth: {
-    // user: serverEnv.GMAIL_USER,
-    // pass: serverEnv.GMAIL_APP_PASSWORD,
-  },
-});
+//   auth: {
+//     // user: serverEnv.GMAIL_USER,
+//     // pass: serverEnv.GMAIL_APP_PASSWORD,
+//   },
+// });
