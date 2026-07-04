e38390d1fb47732e10a10fe1ba1f79a963ea93ef
johnmal2012 <www.johnmal@gmail.com>
2026-07-04 18:29:29 -0400

    update placeholder message

--- src/components/profile/profile-form.tsx
+++ src/components/profile/profile-form.tsx
@@ -392,7 +392,7 @@ export function ProfileForm({
         <div>
           <p className="text-sm text-muted-foreground ml-2.5">Expertise</p>
           <Input
-            placeholder="comma separated e.g. Sports Injuries, Foot Surgery, etc"
+            placeholder="Separate each item with a comma (e.g., Sports Injuries, Foot Surgery, Knee Pain)"
             value={formData.expertise}
             onChange={(e) => updateField('expertise', e.target.value)}
           />
