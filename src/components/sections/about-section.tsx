e2da135c9a86204322a788824cb77ef104169527
johnmal2012 <www.johnmal@gmail.com>
2026-08-30 09:52:54 -0400

    fix about section picture 1/3

--- src/components/sections/about-section.tsx
+++ src/components/sections/about-section.tsx
@@ -31,22 +31,21 @@ export default async function AboutSection({
 }: AboutSectionProps) {
   const mdx = await renderMDX(content);
   return (
-    <section
-      id={slug}
-      className={cn("scroll-mt-28 px-6 py-12", background,)}
-    >
+    <section id={slug} className={cn('scroll-mt-28 px-6 py-12', background)}>
       <div className="mx-auto grid max-w-7xl gap-12 md:grid-cols-3 md:items-center">
         {/* Doctor's photograph: circle */}
-        {/*<div className="relative w-64 h-64 mx-auto md:w-80 md:h-80 rounded-full overflow-hidden shadow-2xl border-4 border-white">
-          <Image
-            src={image}
-            alt={name}
-            width={600}
-            height={700}
-            priority
-            className="rounded-3xl object-cover shadow-2xl"
-          />
-        </div>*/}
+        {/* <div className="mx-auto w-1/2 max-w-sm md:w-full">
+          <div className="relative w-64 h-64 mx-auto md:w-80 md:h-80 rounded-full overflow-hidden shadow-2xl border-4 border-white">
+            <Image
+              src={image}
+              alt={name}
+              width={600}
+              height={700}
+              priority
+              className="rounded-3xl object-cover shadow-2xl"
+            />
+          </div>
+        </div> */}
         {/* Doctor's photograph: square protrait */}
         <div className="mx-auto w-1/2 max-w-sm md:w-full">
           <div className="overflow-hidden rounded-3xl border-4 border-white bg-white shadow-2xl">
@@ -94,7 +93,7 @@ export default async function AboutSection({
               {/* <Separator className="data-[orientation=horizontal]:h-1 bg-slate-300" /> */}
               <Separator />
             </div>
-            <div className="prose max-w-none" >{mdx}</div>
+            <div className="prose max-w-none">{mdx}</div>
           </div>
         </Card>
       </div>
